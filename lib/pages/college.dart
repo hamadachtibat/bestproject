0920c9676628db33a3d79d915faa2ebeb4dad508
hamadachtibat <69968609+hamadachtibat@users.noreply.github.com>
2020-08-30 21:21:43 +0100

    ad some files

--- lib/pages/college.dart
+++ lib/pages/college.dart
@@ -1,5 +1,5 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter_alfadl2/pages/1annecollege.dart';
 import 'package:flutter_alfadl2/pages/2emeannecollege.dart';
 import 'package:flutter_alfadl2/pages/3annecollege.dart';
@@ -30,149 +30,167 @@ class _collegelistState extends State<collegelist> {
               child: Column(
               children: <Widget>[
                 SizedBox(height: 15,),
-                GestureDetector(
-                  onTap:  (){
-                    Navigator.push(context, new MaterialPageRoute(
-                      builder: (context) => firstannecollege(),
-                    ));
+                OpenContainer(
+                  openBuilder: (context,_){
+                    return firstannecollege();
                   },
-                  child: Container(
-                    margin: EdgeInsets.all(8),
-                    height: 150,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        gradient: LinearGradient(
-                          colors: [Colors.white,Colors.tealAccent],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
+                  closedBuilder: (context,VoidCallback openContainer){
+                    return GestureDetector(
+                      onTap:  openContainer,
+                      child: Container(
+                        margin: EdgeInsets.all(8),
+                        height: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(
+                              colors: [Colors.white,Colors.tealAccent],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.tealAccent,
+                                blurRadius: 12,
+                                offset: Offset(0,6),
+                              )
+                            ]
                         ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.tealAccent,
-                            blurRadius: 12,
-                            offset: Offset(0,6),
-                          )
-                        ]
-                    ),
-                     child: Row(
-                       children: <Widget>[
-                         Image.asset("assets/images/alfadl.png",
+                        child: Row(
+                          children: <Widget>[
+                            Image.asset("assets/images/alfadl.png",
                             ),
 
-                         Column(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           crossAxisAlignment: CrossAxisAlignment.center,
-                           children: <Widget>[
-                             Text("1.A.C.",
-                             style: TextStyle(
-                               fontSize: 23,
-                             ),),
-                             SizedBox(height: 10,),
-                             Image.asset("assets/images/college-icon-png-6.jpg",
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text("1.A.C.",
+                                  style: TextStyle(
+                                    fontSize: 23,
+                                  ),),
+                                SizedBox(height: 10,),
+                                Image.asset("assets/images/college-icon-png-6.jpg",
 
-                             width:60,),
-                           ],
-                         )
-                       ],
-                     ),
-                  ),
+                                  width:60,),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  transitionType: ContainerTransitionType.fadeThrough,
+                  transitionDuration: Duration(milliseconds: 1000),
+                  closedColor: Colors.purple,
                 ),
                 SizedBox(height: 20,),
-                GestureDetector(
-                  onTap:  (){
-                    Navigator.push(context, new MaterialPageRoute(
-                      builder: (context) => secondannecollege(),
-                    ));
+                OpenContainer(
+                  openBuilder: (context,_){
+                    return secondannecollege();
                   },
-                  child: Container(
-                    margin: EdgeInsets.all(8),
-                    height: 150,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        gradient: LinearGradient(
-                          colors: [Colors.white,Colors.tealAccent],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
+                  closedBuilder: (context,VoidCallback openContainer){
+                    return GestureDetector(
+                      onTap:  openContainer,
+                      child: Container(
+                        margin: EdgeInsets.all(8),
+                        height: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(
+                              colors: [Colors.white,Colors.tealAccent],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.tealAccent,
+                                blurRadius: 12,
+                                offset: Offset(0,6),
+                              )
+                            ]
                         ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.tealAccent,
-                            blurRadius: 12,
-                            offset: Offset(0,6),
-                          )
-                        ]
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        Image.asset("assets/images/alfadl.png",
-                        ),
-
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                        child: Row(
                           children: <Widget>[
-                            Text("2.A.C.",
-                              style: TextStyle(
-                                fontSize: 23,
-                              ),),
-                            SizedBox(height: 10,),
-                            Image.asset("assets/images/college-icon-png-6.jpg",
+                            Image.asset("assets/images/alfadl.png",
+                            ),
 
-                              width:60,),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text("2.A.C.",
+                                  style: TextStyle(
+                                    fontSize: 23,
+                                  ),),
+                                SizedBox(height: 10,),
+                                Image.asset("assets/images/college-icon-png-6.jpg",
+
+                                  width:60,),
+                              ],
+                            )
                           ],
-                        )
-                      ],
-                    ),
-                  ),
+                        ),
+                      ),
+                    );
+                  },
+                  transitionType: ContainerTransitionType.fadeThrough,
+                  transitionDuration: Duration(milliseconds: 1000),
+                  closedColor: Colors.purple,
                 ),
                 SizedBox(height: 20,),
-                GestureDetector(
-                  onTap:  (){
-                    Navigator.push(context, new MaterialPageRoute(
-                      builder: (context) => thirdannecollege(),
-                    ));
+                OpenContainer(
+                  openBuilder: (context,_){
+                    return thirdannecollege();
                   },
+                  transitionType: ContainerTransitionType.fadeThrough,
+                  transitionDuration: Duration(milliseconds: 1000),
+                  closedColor: Colors.purple,
+                  closedBuilder: (context,VoidCallback openContainer){
+                    return GestureDetector(
+                      onTap:  openContainer,
 
-                  child: Container(
-                    margin: EdgeInsets.all(8),
-                    height: 150,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        gradient: LinearGradient(
-                          colors: [Colors.white,Colors.tealAccent],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
+                      child: Container(
+                        margin: EdgeInsets.all(8),
+                        height: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(
+                              colors: [Colors.white,Colors.tealAccent],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.tealAccent,
+                                blurRadius: 12,
+                                offset: Offset(0,6),
+                              )
+                            ]
                         ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.tealAccent,
-                            blurRadius: 12,
-                            offset: Offset(0,6),
-                          )
-                        ]
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        Image.asset("assets/images/alfadl.png",
-                        ),
-
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                        child: Row(
                           children: <Widget>[
-                            Text("3.A.C",
-                              style: TextStyle(
-                                fontSize: 23,
-                              ),),
-                            SizedBox(height: 10,),
-                            Image.asset("assets/images/college-icon-png-6.jpg",
+                            Image.asset("assets/images/alfadl.png",
+                            ),
 
-                              width:60,),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text("3.A.C",
+                                  style: TextStyle(
+                                    fontSize: 23,
+                                  ),),
+                                SizedBox(height: 10,),
+                                Image.asset("assets/images/college-icon-png-6.jpg",
+
+                                  width:60,),
+                              ],
+                            )
                           ],
-                        )
-                      ],
-                    ),
-                  ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ],
           ),
